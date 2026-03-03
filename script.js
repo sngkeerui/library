ef15fd5e2d22b3bde17e6b411d907e046c522001
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2026-03-03 13:31:37 +0800

    feat: add book constructor function

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary() {
+    
+}
