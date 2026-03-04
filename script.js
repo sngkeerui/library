aa7901a1fd9f48d0fb8206648990430438f19cbe
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2026-03-04 10:11:20 +0800

    refactor: automate unique id generation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Book(title, author, pages, read, id) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.id = id;
+    this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary() {
