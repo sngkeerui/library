81caa05767d3471512e844c0e427d7ce52c201f3
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2026-03-26 14:51:02 +0800

    refactor: use class syntax for Book object

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = Number(pages);
     this.read = read ? "Read" : "Not Read";
     this.id = crypto.randomUUID();
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
